f3d5f37cffc800752922c9f8cf00400d8c8fe8b7
HanReez <farhanharis1@gmail.com>
2022-04-12 00:31:14 +0800

    change from Tenant to student :)

--- SEWAWEH3/renthouse/how-to-register.php
+++ SEWAWEH3/renthouse/how-to-register.php
@@ -15,7 +15,7 @@ include("navbar.php");
                 <div class="col-md-12 sign-up" style="text-align: center;">
                     <h3 style="font-weight: bold;">How do you want to Register?</h3><hr>
                     <p>If you want to register as a tenant click on tenant register button otherwise click on owner register button.</p><br><br>
-                    <button type="submit" class="btn btn-info"  onclick="window.location.href='tenant-register.php'" style="width:200px;">Tenant Register</button>
+                    <button type="submit" class="btn btn-info"  onclick="window.location.href='tenant-register.php'" style="width:200px;">Student Register</button>
                     <button type="submit" class="btn btn-info"  onclick="window.location.href='owner-register.php'" style="width:200px;">Owner Register</button>
                 </div>
                 
